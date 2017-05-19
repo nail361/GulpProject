d6f56607129dad3e595dd2dbb35494d23eebe4bd
unknown <balyasnikov.ds@243-007-1.academy.ru>
2017-05-19 17:35:33 +0300

    game update

--- gulpfile.js
+++ gulpfile.js
@@ -4,18 +4,20 @@ var babelify    = require('babelify');
 var source      = require('vinyl-source-stream');
 var buffer      = require('vinyl-buffer');
 var uglify      = require('gulp-uglify');
+var cssmin      = require('gulp-cssmin');
+var htmlmin     = require('gulp-minify-html');
 var sourcemaps  = require('gulp-sourcemaps');
 var livereload  = require('gulp-livereload');
 var sass		= require('gulp-sass');
 var plumber 	= require('gulp-plumber');
- 
+
 gulp.task('build', function () {
     return browserify({entries: './src/js/main.js', debug: true})
         .transform("babelify", { presets: ["es2015"] })
         .bundle()
-		.pipe(plumber({
-			errorHandler: onError
-		}))
+    		.pipe(plumber({
+    			errorHandler: onError
+    		}))
         .pipe(source('app.js'))
         .pipe(buffer())
         .pipe(sourcemaps.init())
@@ -31,18 +33,26 @@ gulp.task('sass', function(){
 			errorHandler: onError
 		}))
 		.pipe(sass())
+        .pipe(cssmin())
 		.pipe(gulp.dest('./build/css'))
 });
- 
+
+gulp.task('html', function(){
+	gulp.src('./src/*.html')
+        .pipe(htmlmin())
+		.pipe(gulp.dest('./build'))
+});
+
 gulp.task('watch', ['build'], function () {
     livereload.listen();
     gulp.watch('./src/js/*.js', ['build']);
-	gulp.watch('./src/scss/**/*.scss', ['sass']); 
+	gulp.watch('./src/scss/**/*.scss', ['sass']);
+    gulp.watch('./src/*.html', ['html']);
 });
- 
-gulp.task('default', ['build', 'sass', 'watch']);
 
-var onError = function (err) {  
+gulp.task('default', ['build', 'sass', 'html', 'watch']);
+
+var onError = function (err) {
   gutil.beep();
   console.log(err.toString());
 };
--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,12 @@
 class Main{
 
     constructor(){
-        const BOARD_SIZE = 20;
+        const BOARD_SIZE = 30;
         const SYMBOLS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
+        this.savedCard = null;
         this.memory_array = new Array(BOARD_SIZE-1);
+        this.busy = false;
 
         let freeSlotsIndex = new Array(BOARD_SIZE-1);
 
@@ -31,14 +33,53 @@ class Main{
 
     Init(){
         this.board = document.querySelector('[data-board]');
-        this.card = this.board.querySelector('[data-item]');
+        this.card = this.board.querySelector('[data-card]');
         this.card.remove();
 
+        this.board.addEventListener('click', this.CardOpen);
+
         this.memory_array.forEach( (symbol, index)=> {
-                this.card.setAttribute("data-item", index);
+                this.card.setAttribute("data-card", index);
                 this.card.querySelector('.card-back').innerHTML = symbol;
                 this.board.insertAdjacentHTML("beforeEnd", this.card.outerHTML);
         });
+    }
+
+    CardOpen(event){
+        if (this.busy) return;
+        if(event.target.hasAttribute('data-card-front')){
+
+            if (this.savedCard){
+
+                let curCard = event.target.closest('[data-card]');
+
+                // if (this.savedCard.getAttribute('[data-card]') == curCard.getAttribute('[data-card]')) return;
+
+                curCard.classList.add('flip');
+                let firstChair = curCard.querySelector('[data-card-back]').innerHTML;
+                let secondChair = this.savedCard.querySelector('[data-card-back]').innerHTML;
+
+                this.busy = true;
+                setTimeout( ()=> {
+                    if (firstChair == secondChair){
+                        curCard.querySelector('[data-card-back]').classList.add('completed');
+                        this.savedCard.querySelector('[data-card-back]').classList.add('completed');
+                    }
+                    else{
+                        this.savedCard.classList.remove('flip');
+                        curCard.classList.remove('flip');
+                    }
+
+                    this.savedCard = null;
+                    this.busy = false;
+                }, 1000);
+
+                return;
+            }
+
+            this.savedCard = event.target.closest('[data-card]');
+            this.savedCard.classList.toggle('flip');
+        }
     }
 
 }
