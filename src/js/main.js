c6f384424262ea0d9d9571026e4f5dbc9dcd1c7d
unknown <balyasnikov.ds@243-007-1.academy.ru>
2017-05-22 16:30:49 +0300

    game update

--- src/js/main.js
+++ src/js/main.js
@@ -1,42 +1,51 @@
 class Main{
 
     constructor(){
-        const BOARD_SIZE = 30;
         const SYMBOLS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
+        this.BOARD_SIZE = 0;
+
         this.savedCard = null;
-        this.memory_array = new Array(BOARD_SIZE-1);
+        this.memory_array = new Array(this.BOARD_SIZE);
         this.busy = false;
+        this.compared = 0;
 
-        let freeSlotsIndex = new Array(BOARD_SIZE-1);
+        while(this.BOARD_SIZE == 0){
+            var answer = prompt("Введите размер поля (целое число, кратное двум)");
 
-        for(let i = 0; i < BOARD_SIZE; i++) freeSlotsIndex[i] = i;
+            if (answer != null) {
+                if(answer%2 != 0 || answer.toString().split('.').length>1)
+                    alert("Число должно быть чётным и целым!");
+                else
+                    this.BOARD_SIZE = answer;
+            }
+        }
 
-        for(let i = 0; i < BOARD_SIZE; i++){
-
-            if (this.memory_array[i] != undefined) continue;
-
+        for(let i = 0; i < this.BOARD_SIZE/2; i++){
             this.memory_array[i] = SYMBOLS.charAt(
                 Math.round( (Math.random()*(SYMBOLS.length-1)) )
             );
-
-            freeSlotsIndex.splice(i,1);
-
-            let pairIndex = Math.round( (Math.random()*(freeSlotsIndex.length-1)) );
-            this.memory_array[freeSlotsIndex[pairIndex]] = this.memory_array[i];
-
-            freeSlotsIndex.splice(pairIndex,1);
+            this.memory_array[this.BOARD_SIZE-1-i] = this.memory_array[i];
         }
+
+        this.ArrayShuffle(this.memory_array);
 
         document.addEventListener("DOMContentLoaded", this.Init.bind(this));
     }
+
+    ArrayShuffle(a) {
+        for (let i = a.length; i; i--) {
+            let j = Math.floor(Math.random() * i);
+            [a[i - 1], a[j]] = [a[j], a[i - 1]];
+        }
+    };
 
     Init(){
         this.board = document.querySelector('[data-board]');
         this.card = this.board.querySelector('[data-card]');
         this.card.remove();
 
-        this.board.addEventListener('click', this.CardOpen);
+        this.board.addEventListener('click', this.CardOpen.bind(this));
 
         this.memory_array.forEach( (symbol, index)=> {
                 this.card.setAttribute("data-card", index);
@@ -53,17 +62,23 @@ class Main{
 
                 let curCard = event.target.closest('[data-card]');
 
-                // if (this.savedCard.getAttribute('[data-card]') == curCard.getAttribute('[data-card]')) return;
+                if (this.savedCard.getAttribute('data-card') == curCard.getAttribute('data-card')) return;
 
                 curCard.classList.add('flip');
                 let firstChair = curCard.querySelector('[data-card-back]').innerHTML;
                 let secondChair = this.savedCard.querySelector('[data-card-back]').innerHTML;
 
                 this.busy = true;
-                setTimeout( ()=> {
+                setTimeout( () => {
                     if (firstChair == secondChair){
                         curCard.querySelector('[data-card-back]').classList.add('completed');
                         this.savedCard.querySelector('[data-card-back]').classList.add('completed');
+
+                        this.compared += 2;
+
+                        if (this.compared == this.BOARD_SIZE){
+                            alert('Поздравляем, вы нашли все соответствия!');
+                        }
                     }
                     else{
                         this.savedCard.classList.remove('flip');
